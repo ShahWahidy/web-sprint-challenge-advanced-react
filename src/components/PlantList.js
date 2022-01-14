569ac54012217272ae32d5887d8357c5ac71edb1
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-14 08:43:02 -0800

    fixed the axios call

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -10,6 +10,18 @@ export default class PlantList extends Component {
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
 
+  //fetching data from the server and setting state
+  componentDidMount(){
+    axios.get(`http://localhost:3333/plants`)
+    .then((res) => {
+      console.log(res)
+      this.setState({
+        ...this.state,
+        plants: res.data,
+      })
+    })
+  }
+
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
