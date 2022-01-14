370bf9321f67f0913506ed0e811d5c730ffad370
Ahmadshah Wahidy <shah.wahidy@gmail.com>
2022-01-14 09:53:58 -0800

    built the hooks

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const initialValue = {
   firstName: "",
@@ -13,18 +14,22 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+// const CheckoutForm = (props) => {
+//   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+//   const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+//   const handleChanges = (e) => {
+//     setValues({ ...values, [e.target.name]: e.target.value });
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setShowSuccessMessage(true);
+//   };
+
+  const CheckoutForm = () =>{
+    const[values, showSuccessMessage, handleChanges, handleSubmit] = useForm();
+  
 
   return (
     <>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,27 @@
-// write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const initialValue = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+  };
+
+const useForm = (initialValue) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+  
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setShowSuccessMessage(true);
+    };
+  }
+
+export default useForm;
+
